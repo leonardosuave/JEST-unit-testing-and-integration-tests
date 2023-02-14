843de70ea605960ba8cb987914d51cde97ff9f11
Leonardo Suave <leonardo.suave@aoatechpartners.com.br>
2023-02-14 20:20:45 -0300

    feat: use 'it' to start a test, describe to create a test´s group and test:watch and test:coverage to generate reports

--- folhaDePagamento.test.js
+++ folhaDePagamento.test.js
@@ -1,16 +1,18 @@
 /* eslint-disable linebreak-style */
 import { somaHorasExtras, calculaDescontos } from './index';
 
-test('Should be returned a extra hour sum', () => {
-  const esperado = 2500;
-  const retornado = somaHorasExtras(2000, 500);
+describe('Tests to paper calcule', () => {
+  it('Should be returned a extra hour sum', () => {
+    const esperado = 2500;
+    const retornado = somaHorasExtras(2000, 500);
 
-  expect(retornado).toBe(esperado);
-});
+    expect(retornado).toBe(esperado);
+  });
 
-test('Should be discount salary`s value', () => {
-  const esperado = 2300;
-  const retornado = calculaDescontos(2500, 2r00);
+  it('Should be discount salary`s value', () => {
+    const esperado = 2300;
+    const retornado = calculaDescontos(2500, 200);
 
-  expect(retornado).toBe(esperado);
+    expect(retornado).toBe(esperado);
+  });
 });
