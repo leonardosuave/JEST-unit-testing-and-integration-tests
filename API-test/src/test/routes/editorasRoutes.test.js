b3e4b3bc991b5e52141741931e969cdb9805b885
Leonardo Suave <leonardo.suave@aoatechpartners.com.br>
2023-02-25 18:40:06 -0300

    it.each to PUT route and name's field to description test

--- API-test/src/test/routes/editorasRoutes.test.js
+++ API-test/src/test/routes/editorasRoutes.test.js
@@ -20,11 +20,11 @@ describe('GET in /editoras', () => {
             .expect('content-type', /json/)
             .expect(200);
 
-        expect(response.body[1].email).toEqual('m@m.com'); // O email é especificado após saber o que deve receber
+        expect(response.body[0].email).toEqual('e@e.com'); // O email é especificado após saber o que deve receber
     });
 });
 
-let getIdResponse
+let getIdResponse;
 describe('POST in /editoras', () => {
     it('Should add a new publishing', async () => {
         const getPublishCreated = await request(app)
@@ -40,18 +40,60 @@ describe('POST in /editoras', () => {
         //It used to pass a params to delete/get in next test;
         getIdResponse = getPublishCreated.body.content.id; 
     });
-});
 
-describe('DELETE in /editoras/id', () => {
-    it('Should delete a publishing created in test before', async () => {
+    it('Shouldn`t add nothing when empty body', async () => {
         await request(app)
-            .delete(`/editoras/${getIdResponse}`)
-            .expect(200);
+            .post('/editoras')
+            .send({})
+            .expect(400);
     });
 });
 
 describe('GET in /editoras/id', () => {
     it('Should return the data selected by id', async () => {
+        await request(app)
+            .get(`/editoras/${getIdResponse}`)
+            .expect(200);
+    });
+});
+
+describe('PUT in /editoras/id', () => {
+
+    // Pode ser feito o test como se fosse um POST quando atualizado apenas um campo ou it.each para modificação de mais campos
+    it.skip('Should update name`s field', async () => {
+        await request(app)
+            .put(`/editoras/${getIdResponse}`)
+            .send({ nome: 'CASA DO CODIGO987456'})
+            .expect(204);
+    });
+
+    // Usar each faz o teste com cada laço do array, passando indice por indice que esta como objeto
+    it.skip.each([
+        { nome: 'CASA DO CODIGO987456'},  // Primeiro
+        { cidade: 'Franca'},        // Segundo
+        { email: 'leo@leo.com'},     // Terceiro
+    ])('Should update all fields by object`s array', async (params) => {
+        await request(app)
+            .put(`/editoras/${getIdResponse}`)
+            .send(params)
+            .expect(204);
+    });
+
+    // Utiliza o objeto do array dentro de arrays separados para referenciar o nome dos campos a serem editados no nome do teste através do %s 
+    it.each([
+        ['nome',{ nome: 'CASA DO CODIGO987456'}],  
+        ['cidade',{ cidade: 'Franca'}],
+        ['email',{ email: 'leo@leo.com'}],
+    ])('Should update %s field', async (chave, param) => {
+        await request(app)
+            .put(`/editoras/${getIdResponse}`)
+            .send(param)
+            .expect(204);
+    });
+});
+
+describe('DELETE in /editoras/id', () => {
+    it('Should delete a publishing created in test before', async () => {
         await request(app)
             .delete(`/editoras/${getIdResponse}`)
             .expect(200);
