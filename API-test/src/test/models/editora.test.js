332c7bad80f44fd3caad6e100530ee616e389a97
Leonardo Suave <leonardo.suave@aoatechpartners.com.br>
2023-02-25 12:41:06 -0300

    Use mock to test a GET API call without save new datas on database

--- API-test/src/test/models/editora.test.js
+++ API-test/src/test/models/editora.test.js
@@ -1,3 +1,4 @@
+import { jest } from '@jest/globals';
 import Editora from '../../models/editora';
 
 describe('Should test publishing model', () => {
@@ -25,13 +26,37 @@ describe('Should test publishing model', () => {
         }); 
     });
 
-    it('Should save publishing in database by async/await', async () => {
+    it.skip('Should save publishing in database by async/await', async () => {
         const publishing = new Editora(objetoEditora);
         const getResult = await publishing.salvar();
         const resultReturned = await Editora.pegarPeloId(getResult.id)
 
         expect(getResult.nome).toBe('CDC');
         expect(resultReturned).toEqual(
+            expect.objectContaining({
+                id: expect.any(Number),
+                ...objetoEditora,
+                created_at: expect.any(String),
+                updated_at: expect.any(String)
+            })
+        );    
+    });
+
+    it('Sould simulate a call to database', () => {
+        const editora = new Editora(objetoEditora);
+
+        editora.salvar = jest.fn().mockReturnValue({
+            id: 10,
+            nome: 'CDC',
+            cidade: 'Sao Paulo',
+            email: 'c@c.com',
+            created_at: '2022-10-01',
+            updated_at: '2022-10-01'
+        });
+
+        const getReturn = editora.salvar();
+
+        expect(getReturn).toEqual(
             expect.objectContaining({
                 id: expect.any(Number),
                 ...objetoEditora,
